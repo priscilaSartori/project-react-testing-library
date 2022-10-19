c10cdb9e2caf2f4b2f6a76c3d045e5cb7bec947a
Priscila <priscilasartori2@gmail.com>
2022-10-19 19:46:48 -0300

    Requisito 1

---
+++ src/tests/App.test.js
@@ -0,0 +1,50 @@
+import { MemoryRouter, Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+const { screen, render } = require('@testing-library/react');
+
+test('Se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const linkHome = screen.getByRole('link', { name: /Home/i });
+  expect(linkHome).toBeInTheDocument();
+  const linkAbout = screen.getByRole('link', { name: /About/i });
+  expect(linkAbout).toBeInTheDocument();
+  const linkFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+  expect(linkFavorite).toBeInTheDocument();
+});
+
+test('Se a aplicação é redirecionada para a página inicial, na URL / ao clicar no link Home da barra de navegação', () => {
+  const { history } = renderWithRouter(<App />);
+  const linkHome = screen.getByRole('link', { name: /Home/i });
+  userEvent.click(linkHome);
+  expect(history.location.pathname).toBe('/');
+});
+
+test('Se a aplicação é redirecionada para a página de About, na URL / about, ao clicar no link About da barra de navegação', () => {
+  const { history } = renderWithRouter(<App />);
+  const linkAbout = screen.getByRole('link', { name: /About/i });
+  userEvent.click(linkAbout);
+  expect(history.location.pathname).toBe('/about');
+});
+
+test('Se a aplicação é redirecionada para a página de Pokémons Favoritados, na URL / favorites, ao clicar no link Favorite Pokémons da barra de navegação', () => {
+  const { history } = renderWithRouter(<App />);
+  const linkFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+  userEvent.click(linkFavorite);
+  expect(history.location.pathname).toBe('/favorites');
+});
+
+test('Se a aplicação é redirecionada para a página Not Found ao entrar em uma URL desconhecida', () => {
+  const { history } = renderWithRouter(<App />);
+  act(() => history.push('/xablau'));
+  const notFoundText = screen.getByRole('heading', { name: /Page requested not found/i });
+  expect(notFoundText).toBeInTheDocument();
+});
