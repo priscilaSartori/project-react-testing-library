5a8b835d7d4f6bd3047830030c4a7c9a922dbad1
Priscila <priscilasartori2@gmail.com>
2022-10-20 23:50:50 -0300

    Requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -9,7 +9,7 @@ const { screen } = require('@testing-library/react');
 const pokemon = pokemons[0];
 const pokemon25 = '/pokemons/25';
 
-test('Teste se é renderizado um card com as informações de determinado pokémon', () => {
+test('se é renderizado um card com as informações de determinado pokémon', () => {
   renderWithRouter(
     <Pokemon pokemons={ pokemons } pokemon={ pokemon } />,
   );
@@ -34,29 +34,17 @@ test('se o card do pokémon indicado na Pokédex contém um link de navegação 
   expect(linkMoreDetails).toHaveAttribute('href', pokemon25);
 });
 
-test('Teste se ao clicar no link de navegação do pokémon, é feito o redirecionamento da aplicação para a página de detalhes de pokémon', () => {
-  const { history } = renderWithRouter(
-    <Pokemon pokemons={ pokemons } pokemon={ pokemon } />,
-  );
-  const linkMoreDetails = screen.getByRole('link', { name: /More details/i });
-  userEvent.click(linkMoreDetails);
-  expect(history.location.pathname).toBe(pokemon25);
-});
-
-test('Teste também se a URL exibida no navegador muda para /pokemon/<id>, onde <id> é o id do pokémon cujos detalhes se deseja ver', () => {
+test('se ao clicar no link de navegação do pokémon, é feito o redirecionamento da aplicação para a página de detalhes de pokémon e também se a URL exibida no navegador muda para /pokemon/<id>, onde <id> é o id do pokémon cujos detalhes se deseja ver', () => {
   const { history } = renderWithRouter(
     <Pokemon pokemons={ pokemons } pokemon={ pokemon } />,
   );
   const linkMoreDetails = screen.getByRole('link', { name: /More details/i });
   userEvent.click(linkMoreDetails);
   act(() => history.push(pokemon25));
-  // expect(history.location.pathname).toBe(pokemon25);
-
-  // const imgagemPokemon = screen.getByAltText('Pikachu sprite');
   expect(history.location.pathname).toBe(pokemon25);
 });
 
-test('Teste se existe um ícone de estrela nos pokémons favoritados', () => {
+test('se existe um ícone de estrela nos pokémons favoritados', () => {
   const { history } = renderWithRouter(
     <Pokemon
       pokemons={ pokemons }
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+test('se as informações detalhadas do pokémon selecionado são mostradas na tela', () => {
+// A página deve conter um texto < name > Details, onde < name > é o nome do pokémon;
+// Não deve existir o link de navegação para os detalhes do pokémon selecionado;
+// A seção de detalhes deve conter um heading h2 com o texto Summary;
+// A seção de detalhes deve conter um parágrafo com o resumo do pokémon específico sendo visualizado.
+// obs: é possível utilizar regex para capturar o parágrafo
+});
+
+test('se existe na página uma seção com os mapas contendo as localizações do pokémon', () => {
+//   Na seção de detalhes deverá existir um heading h2 com o texto Game Locations of < name >; onde < name > é o nome do pokémon exibido;
+//   Todas as localizações do pokémon devem ser mostradas na seção de detalhes;
+// Devem ser exibidos o nome da localização e uma imagem do mapa em cada localização;
+// A imagem da localização deve ter um atributo src com a URL da localização;
+// A imagem da localização deve ter um atributo alt com o texto < name > location, onde < name > é o nome do pokémon.
+});
+
+test('se o usuário pode favoritar um pokémon através da página de detalhes', () => { });
